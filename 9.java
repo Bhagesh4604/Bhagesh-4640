5b980b05cce1a0ccfc0984ba95906359e5c1f386
Bhagesh4604 <bhageshbiradar820@gmail.com>
2024-12-02 22:45:29 +0530

    Update 9.java

--- 9.java
+++ 9.java
@@ -1,32 +1,33 @@
-public class CustomDivision 
-{
-public static void main(String[] args) { 
-int numerator = 10;
-int denominator = 0;
+package divisionByZero;
+
+public class DivisionByZero {
+public static void main (String[] args) {
+int numerator=10;
+int denominator=0;
 try
 {
-if (denominator == 0)
-{
-throw new DivisionByZeroException("Division by zero is not allowed!");
-}
-int result = numerator / denominator; 
-System.out.println("Result: " + result);
+	if(denominator==0)
+	{
+		throw new DivisionByZeroException("division by zero is not allowed!");
+	}
+	int result=numerator/denominator;
+			System.out.println("Result:" +result);
 }
 catch (DivisionByZeroException e)
 {
-System.err.println("Error: " + e.getMessage());
+	System .err.println("Error:" +e.getMessage());
 }
 finally
 {
-System.out.println("This block always executes, 
-regardless of exceptions.");
+	System.out.println("This block always executes regardless of exception");
 }
+}
+}
+class DivisionByZeroException extends Exception
+{
+	public DivisionByZeroException(String message)
+	{
+		super(message);
+	}
 }
 
-class DivisionByZeroException extends Exception 
-{
-public DivisionByZeroException(String message)
-{
-super(message);
-}
-}
